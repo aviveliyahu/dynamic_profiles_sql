8c0ec82140157c8a3d1142c246c7b0296b0f3f07
Phys3r <aviveliyahu.ae@gmail.com>
2024-03-31 16:41:13 +0300

    change to sql data fetching, html modified to support this as well.

--- profile.js
+++ profile.js
@@ -1,15 +1,21 @@
+// File created by Aviv Eliyahu and Matan Asraf.
 let express = require('express');
 let app = express();
+let mysql = require('mysql');
 let path = require('path');
 let fs = require("fs");
 
 
-const PORT = 3000;
+const NODE_PORT = 3000;
 const BIO_FILE_NAME = "bio.txt";
-const FRIENDS_PATH = path.join(__dirname, "private/");
-const TITLE_FILE_NAME = "title.txt"
-const TEXT_START = "text";
-const TEXT_END = "txt";
+const FRIENDS_PATH = path.join(__dirname, "private/");
+
+
+const URL = "localhost";
+const SQL_PORT = "3306";
+const USERNAME = "root";
+const PASSWORD = "12345";
+const DATABASE = "profiles";
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -17,46 +23,66 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/profile', function(req, res) { 
+app.get('/profile', function (req, res) {
   // get ID entered by user
   let id = req.query.id;
 
-  // get right title & short description by ID
-  let title_file = fs.readFileSync(`private/${id}/${TITLE_FILE_NAME}`,'utf-8');
-  let title_lines = title_file.split("\n");
+  // get relevant bio file to the id
+  let bio = fs.readFileSync(`private/${id}/${BIO_FILE_NAME}`, { encoding: 'utf-8', flag: 'r' });
+  let bio_lines = bio.split("\n");
 
-
- // get relevant bio file to the id
- let bio = fs.readFileSync(`private/${id}/${BIO_FILE_NAME}`, { encoding: 'utf-8', flag: 'r' });
- let bio_lines = bio.split("\n"); 
-
-
- // get friends list (except the one that the user view his profile)
- let friends = fs.readdirSync(FRIENDS_PATH,{ encoding: 'utf-8', flag: 'r' });
- let filtered_friends = friends.filter(function (friend) {
-   if(friend!==id){
-     return friend;
-   };
- });
-
-  // get files name that start with "text" and ends with "txt"
-  const TEXT_PATH = path.join(__dirname, `private/${id}`);
-  let files = fs.readdirSync(TEXT_PATH,{ encoding: 'utf-8', flag: 'r' });
-  let filtered_files = files.filter(function (text_file){
-    return text_file.split(".")[0].slice(0,4)===TEXT_START && text_file.split(".")[1]===TEXT_END;
+  // get friends list (except the one that the user view his profile)
+  let friends = fs.readdirSync(FRIENDS_PATH, { encoding: 'utf-8', flag: 'r' });
+  let filtered_friends = friends.filter(function (friend) {
+    if (friend !== id) {
+      return friend;
+    };
   });
 
-      // get content of relevant text files
-      let endorsement_files = [];
-      for (let i=0; i< filtered_files.length;i++){
-          let file_name = filtered_files[i];
-          endorsement_files.push(fs.readFileSync(`private/${id}/${file_name}`, { encoding: 'utf-8', flag: 'r' }));
-        };
+  // get right title & short description by ID from our SQL DB
+  // connect to DB
+  const con = mysql.createConnection({
+    host: URL,
+    port: SQL_PORT,
+    database: DATABASE,
+    user: USERNAME,
+    password: PASSWORD
+  });
 
-
- // load the page with relevant filling to the id entered
- res.render('profile', {id,title_lines,bio_lines,filtered_friends,endorsement_files});
+  // recieve relevant information from DB 
+  let title_endor = [];
+  con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connection created.");
+    let parameter = [id];
+    con.query("SELECT ti.title as title,ti.long_text as title_text ,te.long_text as endor, te.signature as signature FROM title ti left join text te on ti.profile = te.profile " +
+      "WHERE ti.profile like ?", parameter,
+      function result(err, result) {
+        if (err) throw err;
+        let i = 0;
+        for (line of result) {
+          if (i === 0) {
+            title_endor.push(line["title"]);
+            title_endor.push(line["title_text"]);
+            title_endor.push(line["endor"]);
+            title_endor.push(line["signature"]);
+            i++;
+          }
+          else {
+            title_endor.push(line["endor"]);
+            title_endor.push(line["signature"]);
+            i++;
+          }
+        }
+        // load the page with relevant filling to the id entered
+        res.render('profile', { id, title_endor, bio_lines, filtered_friends });
+      });
+    con.end(function (err) {
+      if (err) throw err;
+      console.log("Connection terminated.");
+    });
+  });
 });
 
-app.listen(PORT);
-console.log(`Server is listening on port ${PORT}`); 
+app.listen(NODE_PORT);
+console.log(`Server is listening on port ${NODE_PORT}`); 
